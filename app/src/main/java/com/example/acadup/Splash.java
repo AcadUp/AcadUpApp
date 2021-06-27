4dd3d616adacd27179fcda5fcd437fcc9e470f03
AcadUP <AcadUP>
2021-06-27 15:35:29 +0530

    Details activity added

---
+++ app/src/main/java/com/example/acadup/Splash.java
@@ -0,0 +1,23 @@
+package com.example.acadup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,LoginActivity.class));
+finish();
+            }
+        },5000);
+    }
+}
--- app/src/main/java/com/example/acadup/User_details.java
+++ app/src/main/java/com/example/acadup/User_details.java
@@ -1,14 +1,14 @@
 package com.example.acadup;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class welcome extends AppCompatActivity {
+import android.os.Bundle;
+
+public class User_details extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_user_details);
     }
 }
